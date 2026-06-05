a17e46615355ee6ae2fa220dda17b6cca8cab3d1
Thang <Thang@FUSHI>
2026-06-05 21:08:13 +0700

    Them cac chuc nang con lai cua member gom: -Update -Delete -Search
    Note lai 1 so y chinh qtrong
    
    loai bo 1 so import thua

--- src/MemberList.java
+++ src/MemberList.java
@@ -1,6 +1,5 @@
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.Scanner;
 
 
@@ -9,9 +8,9 @@ public class MemberList extends ArrayList<Member>{
     
     
     //input du lieu tu user
-    public Member inputNewMember() {
+    public void addNewMember() {
 
-    Scanner sc = new Scanner(System.in);//tao scanner de lay input tu 
+    Scanner sc = new Scanner(System.in);//tao scanner de lay input tu user
 
     System.out.println("----------- ADD MEMBER -----------");
 
@@ -21,25 +20,36 @@ public class MemberList extends ArrayList<Member>{
     System.out.print("Name: ");
     String name = sc.nextLine();
 
-    System.out.print("Phone: ");
+    System.out.print("Phone Number: ");
     String phone = sc.nextLine();
 
     System.out.print("Email: ");
-     String email = sc.nextLine();
-
-    return new Member(id, name, phone, email);
+    String email = sc.nextLine();
+    
+    System.out.println("[1] Save    [2] Cancel");
+    System.out.print("Choose: ");
+    int choice = sc.nextInt();
+    if (choice == 1) {
+        System.out.println("Member added successfully!");
+        Member newMember = new Member(id, name, phone, email);//tao ra object vao member moi
+        this.add(newMember);//them new member vao arraylist
+    } else {
+        System.out.println("Operation cancelled!");
+    } 
 }   
-    //them new member vao arraylist
-     public void addMember(Member newMember)
-    {   
-        this.add(newMember);
-        System.out.println("New member added successfully!");
-    }
+
+    
+     
+     
      
      //Display member
      public void displayMember()
     {
         int size = this.size();
+        if (size == 0) {
+            System.out.println("No available member.");
+            return;
+        }
         for (int i = 0; i < size; i++) {
             Member member = this.get(i);
             System.out.println(member.toString());
@@ -47,27 +57,107 @@ public class MemberList extends ArrayList<Member>{
     }
     
     
+    
     public void updateMember()
     {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("----------- UPDATE MEMBER -----------");
+        System.out.println("Enter member ID: ");
+        String updateID = sc.nextLine();
         
+        boolean found = false;
+        int size = this.size();
+        for (int i = 0; i < size; i++) {
+            Member member = this.get(i);
+            if (member.getMemberID().equalsIgnoreCase(updateID)) {
+            found = true;
+            System.out.println("Current Information: " + member.toString());
+            
+            
+            System.out.print("New Member ID: ");
+            String newID = sc.nextLine();//nhap input gia tri moi vao
+            member.setMemberID(newID);//gan gia tri moi vao member va tu member do se gan vao memberList moi
+
+            System.out.print("New Name: ");
+            String newName = sc.nextLine();
+            member.setMemberID(newName);
+
+            System.out.print("New Phone Number: ");
+            String newPhoneNumber = sc.nextLine();
+            member.setMemberID(newPhoneNumber);
+
+            System.out.print("Email: ");
+            String newEmail = sc.nextLine();
+            member.setMemberID(newEmail);
+            
+            break;//dung vonglap lun de k con phai chay them 1 vong moi nua de tiet kiem tai nguyen va thoi gian
+            }
+            
+        }
+        if (!found) {
+            System.out.println("Member not found.");
+        }
     }
     
-    public String searchMember(String name) {
+    
+    
+    public void searchMember() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("----------- SEARCH MEMBER -----------");
+        System.out.println("Enter member ID: ");
+        String searchID = sc.nextLine();
         
-        return null;
+        boolean found = false;
+        int size = this.size();
+        for (int i = 0; i < size; i++) {
+            Member member = this.get(i);
+            if (member.getMemberID().equalsIgnoreCase(searchID)) {
+                found = true;
+                System.out.println("Member found: ");
+                System.out.println(member.toString());
+                 break;       
+                
+            }
+        }
+        if (!found) {
+            System.out.println("Member not found.");
+        } 
+    }
+    
+    public void deleteMember() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("----------- DELETE MEMBER -----------");
+        System.out.println("Enter member ID: ");
+        String searchID = sc.nextLine();
         
+        Member removeMember = null;//tao object removeMember = null de khi ma tim thay id cua member thi se gan object nay vao member dc tim thay
+        
+        int size = this.size();
+        for (int i = 0; i < size; i++) {
+            Member member = this.get(i);
+            if (member.getMemberID().equalsIgnoreCase(searchID)) {
+                removeMember = member;// neu da tim dc member thanh cong thi se gan member dc tim thay do vao object removeMember de dinh danh member do de de hon trong vjec delete member
+                System.out.println("Member found: ");
+                System.out.println(member.toString());
+                break;        
+            }
+        }
+        if (removeMember != null) {
+            System.out.println("[1] Save    [2] Cancel");
+            System.out.print("Choose: ");
+            int choice = sc.nextInt();
+            if (choice == 1) {
+                this.remove(removeMember);//Tu 2 note tren ta co the thay rang trong muc deleteMember can co 1 bien co de xac dinh member do la ai de co the remove de hon
+                System.out.println("Member deleted successfully!");
+            } else {
+                System.out.println("Operation cancelled!");
+            } 
+        } else {
+            System.out.println("Member not found.");
+        }
     }
            
-    //Methods not in abstraction
-    public String getMember()
-    {
-        return Member;
-    }
     
-    public void updateinfo()
-    {
-        
-    }
     
     public int getBorrowLimit()
     {
